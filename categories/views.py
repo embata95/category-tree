e8d2b7c0a6b7ff0aafaebc337fe3917766effb67
Emiliyan <e.a.rashev@abv.bg>
2022-03-15 00:41:31 +0200

    Refactored island view and renamed tests

--- categories/views.py
+++ categories/views.py
@@ -75,34 +75,23 @@ class CategoriesIslands(viewsets.ReadOnlyModelViewSet):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.all_visited_keys = []
+        self.visited_combinations = set()
 
-    def get_island_ids(self, obj, visited_combinations=[]):
-        for curr_node in obj.similar_to.all():
-            ids = [obj.id, curr_node.id]
-            key = f"{min(ids)}_{max(ids)}"
-            if key not in self.all_visited_keys:
-                visited_combinations.append(key)
-                self.all_visited_keys.append(key)
-                self.get_island_ids(curr_node, visited_combinations)
-        ids = (set([int(num) for combination in visited_combinations for num in combination.split('_')]))
-        return None or ids
+    def get_island_ids(self, obj, curr_island=None):
+        if not curr_island:
+            curr_island = set()
+        for curr_node in set(obj.similar_to.all()) - curr_island - self.visited_combinations:
+            curr_island.add(obj)
+            curr_island.add(curr_node)
+            self.visited_combinations.add(obj)
+            self.visited_combinations.add(curr_node)
+            self.get_island_ids(curr_node, curr_island)
+        return curr_island
 
     def list(self, request, *args, **kwargs):
         result = []
         for curr_obj in Category.objects.all():
-            curr_island = self.get_island_ids(curr_obj, [])
-            if curr_island:
-                result.append([Category.objects.filter(id__in=curr_island)])
-        islands = [island for island in result]
-        return Response([{"members": [obj.name for obj in island[0]]} for island in islands])
-
-
-class LongestRabbitHole(viewsets.ModelViewSet):
-    # Again use memoisation to make O(N) complexity
-    # open rabbit holes and closed (circular)
-    # Open holes -> start and end only with one similar
-    # Iterate over all connections and aggregate the straight parts where possible and create a conjunction table
-    # Record all visited nodes and check if any are not visited (should be single nodes not part of rabbit hole or circles)
-    # Again, if no movement available, return -> this will open the closed holes
-    pass
+            island = self.get_island_ids(curr_obj)
+            if island:
+                result.append(island)
+        return Response([{'members': [obj.name for obj in temp_island]} for temp_island in result])
